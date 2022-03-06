4beebbb8f4c95abae4592342af81958e88fa3f16
johnayuk <john1234ayuk@gmail.com>
2022-03-06 16:28:10 +0100

    correction to app

--- staticfiles/staticfiles/staticfiles/static/custom.js
+++
@@ -1,7 +0,0 @@
-let message = document.getElementById('message')
-
-
-setTimeout(function() {
-    $('#message').fadeOut('slow');
-}, 3000)
-
--- staticfiles/staticfiles/staticfiles/staticfiles/staticfiles/api_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description','complete', 'created')
-
-admin.site.register(MyTask,TaskAdmin)
